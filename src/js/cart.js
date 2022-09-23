bacd4443239c89695594c6932fdc99427d4aa959
Tanner Robinson <Rob16041@byui.edu>
2022-09-23 13:21:39 -0600

    Tidy code in cart.js

--- src/js/cart.js
+++ src/js/cart.js
@@ -3,7 +3,6 @@ async function getLocalStorage(key) {
 }
 
 async function getCartContents() {
-  let markup = '';
   const cartItems = await getLocalStorage('so-cart');
   const htmlItems = cartItems.map((item) => renderCartItem(item));
   document.querySelector('.product-list').innerHTML = htmlItems.join('');
@@ -25,7 +24,6 @@ function renderCartItem(item) {
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
 </li>`;
-  console.log(newItem);
   return newItem;
 }
 
