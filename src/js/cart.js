e60baef3a196a730aa58fbecf2cfc683d0589960
Tanner Robinson <Rob16041@byui.edu>
2022-10-18 16:16:15 -0600

    Cart Error fix

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,11 +2,6 @@ async function getLocalStorage(key) {
   return JSON.parse(await localStorage.getItem(key));
 }
 
- HEAD
-async function getCartContents() {
-  const cartItems = getLocalStorage('so-cart');
-  const htmlItems = cartItems.map((item) => renderCartItem(item));
-  document.querySelector('.product-list').innerHTML = htmlItems.join('');
 function displayTotal(total) {
   //display surrounding div (will include checkout button)
   const element = document.querySelector('.cart-footer');
@@ -25,7 +20,6 @@ async function getCartContents() {
     const htmlItems = cartItems.map((item) => renderCartItem(item));
     document.querySelector('.product-list').innerHTML = htmlItems.join('');
   }
-  // document.querySelector(".product-list").innerHTML = renderCartItem(cartItems);
 }
 
 function renderCartItem(item) {
@@ -45,14 +39,4 @@ function renderCartItem(item) {
 </li>`;
   return newItem;
 }
-
-}
-// const cartElement = document.getElementById('cart');
-// let angle = 0;
-
-// setInterval( () => {
-//     angle = (angle + 2) % 360;
-//     squareElement.style.transform = `rotate(${angle}deg)`
-// }, 1000/60);
-// cancelAnimationFrame(id);
 getCartContents();
