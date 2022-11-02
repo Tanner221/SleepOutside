72e1df025630c103f3bf78b5693fc74e2f38031b
christopherleavitt99 <cml95@byui.edu>
2022-11-02 12:52:49 -0600

    added email and minor image css

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,14 @@ const pList = new ProductList("tents", "#product-list", dataSource);
 pList.init();
 
 loadHeaderFooter();
+
+const emails = new EmailTracker("#newsletter");
+
+
+function addEmail(e) {
+    const email = document.querySelector('#email');
+    emails.addEmail({email: email.email});
+}
+
+document.querySelector("#submitButton").addEventListener('click', addEmail);
+
